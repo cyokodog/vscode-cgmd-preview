e72ea9d566a9bf9e39b5c56e60bc1a7bef0a174a
cyokodog <cyokodog@gmail.com>
2023-10-02 14:53:06 +0900

    プレビュー初期表示と保存時表示の実装

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,63 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log(
+    'Congratulations, your extension "vscode-cgmd-preview" is now active!'
+  );
+
+  let webviewPanel = null as null | vscode.WebviewPanel;
+  let webviewPanelDisposed = false;
+
+  const showPreview = () => {
+    if (vscode.window.activeTextEditor) {
+      if (!webviewPanel || webviewPanelDisposed) {
+        webviewPanel = vscode.window.createWebviewPanel(
+          'preview',
+          'preview',
+          vscode.ViewColumn.Two,
+          {
+            enableScripts: true,
+          }
+        );
+        webviewPanelDisposed = false;
+      }
+
+      webviewPanel.onDidDispose(() => {
+        webviewPanelDisposed = true;
+      });
+
+      webviewPanel.webview.html = `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="theme-color" content="#000" />
+      </head>
+      <body>hello${new Date().getMilliseconds()}</body>
+      </html>
+      `;
+    }
+  };
+
+  showPreview();
+
+  const saveDisposable = vscode.workspace.onDidSaveTextDocument((event) => {
+    showPreview();
+  });
+
+  const cmdDisposable = vscode.commands.registerCommand(
+    'vscode-cgmd-preview.preview',
+    () => {
+      vscode.window.showInformationMessage('run vscode-cgmd-preview!');
+    }
+  );
+  context.subscriptions.push(cmdDisposable, saveDisposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
+
+/*
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
@@ -24,3 +84,4 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+*/
